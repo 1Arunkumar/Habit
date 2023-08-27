025bec4747c01846db6de37e36d62a5b0d66cfab
N R Arun Kumar <87988675+1Arunkumar@users.noreply.github.com>
2023-08-27 09:37:14 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const URL = 'mongodb://127.0.0.1:27017/habit_2';
-
+const URL = 'mongodb+srv://arun:NtzLgLyks7iuzCvp@cluster0.1vzl8mv.mongodb.net/habit_2';
 mongoose.connect(URL)
 .then(()=>console.log('MongoDb Is Up And Connected '))
 .catch((error)=>console.log('error connected in Db',error));
